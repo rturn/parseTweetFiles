bf8254d00a19e012a0bb291900431528ec9aa7b8
Exlumberjack <rwturner@wisc.edu>
2015-05-26 00:38:28 -0500

    Fixed description issues

--- R/parseTweetFiles.R
+++ R/parseTweetFiles.R
@@ -90,6 +90,8 @@ mywhich <- function(word.vector, stoplist) {
   word.vector[which(word.vector != "")]
 }
 
+#' Cleans tweet data frames
+#' 
 #' Performs all necessary cleaning on a data frame of tweets. This includes removing all symbols from tweets, converting
 #' them to a lower case array of words, removing all stop words from this array, andconverting timestamps to an 
 #' R usable format. Can also filter by time zone if desired (default does not filter)
@@ -151,6 +153,8 @@ clean.tweets = function(tweets.df, tz = NULL, stoplist = NULL) {
   return(linkless_tweets)
 }
 
+#' Matches tweets to zipcodes
+#' 
 #' Labels an array of geo-located tweets with zip codes using shapefiles provided by the Census Bureau. This function
 #' should be run after cleaning the tweets, and only works on tweets with a latitude / longitude. Any tweets outside the
 #' US are removed before returning the data frame. To run this function the shapefile needs to be downloaded and placed
@@ -196,6 +200,8 @@ locate.tweets = function(located_tweets) {
   return(zipped_tweets)
 }
 
+#' Processes a folder of tweet files
+#' 
 #' Processes all tweet-files in a directory and saves them in the given directory. This functions filters each
 #' files variables, runs clean.tweets on them, and runs locate.tweets on them if desired. 
 #' 
