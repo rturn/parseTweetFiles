2db76226f1d8b423ace4c82581fc21c916aca1f4
Exlumberjack <rwturner@wisc.edu>
2016-02-28 22:20:22 -0600

    Added model fitting functions, updated documentation, fixed package dependencies

--- R/parseTweetFiles.R
+++ R/parseTweetFiles.R
@@ -13,13 +13,18 @@
 #' 
 clean_text = function(x) {
   x = as.character(x)
+  
+  #Remove html tags
   x = gsub("<ed>", "", x)
   x = gsub("+", "", x, fixed = TRUE)
+  #Remove emote leftovers
   x = gsub("<U[0-9a-fA-F]{4}>", "", x)
-  
   y = unlist(strsplit(x, "\\s+"))
-  x = paste(y[!grepl("@|\\.com|\\.org|\\.net|http|RT|-", y)], collapse = " ") #Change these to remove different link artifacts
-
+  #Change these to remove different links
+  x = paste(y[!grepl("^@|\\.com$|\\.org$|\\.net$|^http|\\.edu$", y)], collapse = " ")
+  x = gsub("RT", "", x, fixed = TRUE)
+  
+  #Remove odd symbols and punctuation
   x = sub("&amp;#039;", "", x)
   x = sub("&amp;", "", x)
   x = sub("&gt;", "", x)
@@ -85,7 +90,7 @@ mywhich <- function(word.vector, stoplist) {
 #' @param tz A list of time zones to filter by, currently case sensitive
 #' @param stoplist The stoplist used to filter words 
 #' 
-#' @return The tweet data frame with all editing / filtering done.
+#' @return The tweet data frame with all editing / filtering done. Empty dataset 
 #' 
 #' @examples
 #' \dontrun{df = select(rawdata, text, time_zone)}
@@ -96,27 +101,22 @@ mywhich <- function(word.vector, stoplist) {
 #' @export
 #' 
 clean_tweets = function(tweets.df, tz = NULL, stoplist = NULL) {
-  geo_tweets = tweets.df
-  
+  Sys.setlocale('LC_ALL','C') 
+  #Remove time zones not in tz, return an empty dataset if no tweets from that time zone
+  tweets.df$time_zone == as.character(tweets.df$time_zone)
   if(!is.null(tz)) {
-    geo_tweets$time_zone == as.character(geo_tweets$time_zone)
-    if("NA" %in% tz) {
-      temp = filter(tweets.df, is.na(time_zone))
-    } 
-    temp2 = filter(tweets.df, time_zone %in% tz)   
-    temp = rbind(temp, temp2)
-    if(length(temp[,1]) == 0) {
-      return(temp)
+    tweets.df = dplyr::filter(tweets.df, time_zone %in% tz)   
+    if(length(tweets.df[,1]) == 0) {
+      return(tweets.df)
     }
-    geo_tweets = temp
   }
   
   #Filter links and html codes from text
-  text = unlist(lapply(geo_tweets$text, clean_text))
+  text = unlist(lapply(tweets.df$text, clean_text))
   text = trim(text)
   text = tolower(text)
-  geo_tweets$text = text
-  linkless_tweets = geo_tweets
+  tweets.df$text = text
+  linkless_tweets = tweets.df
   
   #convert factor back to character for future splitting
   linkless_tweets$text <- unlist(lapply(linkless_tweets$text, as.character))
@@ -158,8 +158,8 @@ locate_tweets = function(located_tweets) {
     edited_tweets = data.frame(located_tweets, temp)
     return(edited_tweets)
   }
-  untagged_tweets = filter(located_tweets, is.na(lon) | is.na(lat))
-  tagged_tweets = filter(located_tweets, !is.na(lon) & !is.na(lat))
+  untagged_tweets = dplyr::filter(located_tweets, is.na(lon) | is.na(lat))
+  tagged_tweets = dplyr::filter(located_tweets, !is.na(lon) & !is.na(lat))
   
   if(nrow(tagged_tweets) == 0) {
     temp = rep(NA, length(located_tweets[,1]))
@@ -168,14 +168,14 @@ locate_tweets = function(located_tweets) {
   }
   
   #Shapefile from https://www.census.gov/geo/maps-data/data/cbf/cbf_state.html 
-  areas = readShapeSpatial(system.file("extdata", "cb_2014_us_state_500k.shp", package = "parseTweetFiles"))
+  areas = maptools::readShapeSpatial(system.file("extdata", "cb_2014_us_state_500k.shp", package = "parseTweetFiles"))
   
   #Convert the lon/lat points to coords
   points = data.frame(tagged_tweets$lon, tagged_tweets$lat)
-  points = SpatialPoints(points)
+  points = sp::SpatialPoints(points)
   
   #Overlay the points into the polygons, mapping the coordinates to states
-  statedata = over(points, areas)
+  statedata = sp::over(points, areas)
   
   #Filter the states from the data frame, attach them to the tweets
   state = statedata$STUSPS
@@ -189,33 +189,40 @@ locate_tweets = function(located_tweets) {
 #' Processes a folder of tweet files
 #' 
 #' Processes all tweet-files in a directory and saves them in the given directory. This functions filters each
-#' files variables, runs clean_tweets on them, and runs locate_tweets on them if desired. 
+#' files variables, removes symbols, emotes and stopwords, and tags tweets with zip codes if desired.
 #' 
 #' @param tweetdir The file directory of tweets to read from. No files other than the tweet files should be in this 
 #' directory. The tweet files should be in the format as provided by streamR's parseTweets.
 #' @param outputdir The location to save the tweets to. 
-#' @param loc Determines if the tweets should be zip code tagged. Boolean variable
+#' @param loc True if the tweets should be tagged with zip codes. Uses 2014 shapefile from census.gov 
 #' @param vars The variables to filter from the tweet files.
-#' @param ... tz and stoplist arguments for the inner clean.tweets call. Defaults both to false
+#' @param makedf True if a dataframe containing all the edited tweets should be returned, false otherwise. Defaults
+#' to true.
+#' @param ... tz and stoplist arguments for the inner clean.tweets call.
 #' 
 #' @return The list of filenames edited.
 #'         
 #' @examples
 #' \dontrun{process_files("~/Documents/RawTweets", "~/Documents/EditedTweets")}
 #' \dontrun{process_files("~/Documents/RawTweets", "~/Documents/EditedTweets", loc = TRUE, 
-#' vars = c("text", "time_zone"), tz = c("Jerusalem", "NA"), stoplist = stoplist)}
+#' vars = c("text", "time_zone"), tz = c("Eastern Time (US & Canada)", "NA"), stoplist = stoplist)}
 #' 
 #' @export 
-process_files = function(tweetdir, outputdir, loc = FALSE, vars = "text", ...) {
+process_files = function(tweetdir, outputdir, loc = FALSE, vars = "text", makedf = TRUE, ...) {
+  #Fix a bug with reading some characters from a csv
   Sys.setlocale('LC_ALL','C') 
   filenames = dir(tweetdir)
-  df = data.frame()
-  names(df) = vars
+  
+  #Create the filenames for the edited files
   editedfns = paste(filenames, "e", sep = "")
   existing = dir(outputdir)
   
+  #Don't edit files which have already been edited
   if(length(existing) > 0) {
-    filenames = filenames[-which(editedfns %in% existing)]
+    alreadyEdited = which(editedfns %in% existing)
+    if(length(alreadyEdited) != 0) {
+      filenames = filenames[-alreadyEdited]
+    }
   }
   
   length = length(filenames)
@@ -225,27 +232,38 @@ process_files = function(tweetdir, outputdir, loc = FALSE, vars = "text", ...) {
   }
   
   extras = list(...)
-  for(i in 1:length) {
-    filename = filenames[i]
-    if(file.info(paste(tweetdir, filename, sep = "/"))$size != 0) {
-      tweets.df = read.csv(paste(tweetdir, filename, sep = "/"), header = T)
-      tweets.df = select(tweets.df, one_of(vars))
-      etweets.df = clean_tweets(tweets.df, tz = extras$tz, stoplist = extras$stoplist) 
-      etweets.df = filter(etweets.df, text != "")
-      if(loc == TRUE) {
-        etweets.df = locate_tweets(etweets.df)
-      }
-      df = rbind(df, etweets.df)
-      write.csv(x = etweets.df, file = paste(paste(outputdir, filename, sep = "/"), "e", sep = ""), row.names = FALSE)
+  
+  if(makedf) {
+    df = do.call("rbind", lapply(filenames, manage_tweet_df, tweetdir, outputdir, vars, extras, loc, makedf))
+    return(df)
+  }
+  lapply(filenames, manage_tweet_df, tweetdir, outputdir, vars, extras, loc, makedf)
+  return("Done with no errors")
+}
+
+manage_tweet_df = function(filename, tweetdir, outputdir, vars, extras, loc, makedf) {
+  if(file.info(paste(tweetdir, filename, sep = "/"))$size != 0) {
+    tweets.df = read.csv(paste(tweetdir, filename, sep = "/"), header = T)
+    tweets.df = dplyr::select(tweets.df, dplyr::one_of(vars))
+    etweets.df = clean_tweets(tweets.df, tz = extras$tz, stoplist = extras$stoplist) 
+    etweets.df = dplyr::filter(etweets.df, text != "")
+    if(loc == TRUE) {
+      etweets.df = locate_tweets(etweets.df)
+    }
+    write.csv(x = etweets.df, file = paste(paste(outputdir, filename, sep = "/"), "e", sep = ""), row.names = FALSE)
+    if(makedf) {
+      return(etweets.df)
     }
   }
   
-  return(df)
+  else {
+    return(NULL)
+  }
 }
 
 #' Create a data frame from multiple tweet files
 #'
-#' This function reads in a folder of tweet file then combines them all into one usable data frae
+#' This function reads in a folder of tweet file then combines them all into one usable data frame
 #' 
 #' @param tweetdir The file directory containing the tweet files. No other files should be in this folder.
 #' 
@@ -257,13 +275,86 @@ process_files = function(tweetdir, outputdir, loc = FALSE, vars = "text", ...) {
 #' @export
 make_tweet_df = function(tweetdir) {
   filenames = dir(tweetdir)
-  df = read.csv(paste(tweetdir, filenames[1], sep = "/"), header = T)
-  for(i in 2:length(filenames)) {
-    filename = filenames[i]
-    temp = read.csv(paste(tweetdir, filename, sep = "/"), header = T)
-    df = rbind(df, temp)
-  }
+  df = do.call("rbind", lapply(filenames, read_df_file, tweetdir))
+  #  df = read.csv(paste(tweetdir, filenames[1], sep = "/"), header = T)
+  #  for(i in 2:length(filenames)) {
+  #    filename = filenames[i]
+  #    temp = read.csv(paste(tweetdir, filename, sep = "/"), header = T)
+  #    df = rbind(df, temp)
+  #  }
   return(df)
+}
+
+read_df_file = function(filename, tweetdir) {
+  return(read.csv(paste(tweetdir, filename, sep = "/"), header = T))
+}
+
+#' Fit multiple topic models to successive time intervals using the maptpx package on a time marked Tweet data frame. 
+#' The number of topics will be chosen independently for each interval via Bayes. The model created will be saved to a 
+#' maptpx_model folder in the current directory, and the model is also visualized using the LDAvis package and saved 
+#' into a maptpx_vis folder also in the current directory. Both of these folders need to be created before running the
+#' function. 
+#' 
+#' @param tweets.df The dataframe of time marked tweets to fit models to. Should have a column "created_at" with times in
+#' the posixct format.
+#' @param start.time The first time point to start model fitting.
+#' @param difference The length of time for each time interval (in hours).
+#' @param num.steps The number of time intervals to fit models to. 
+#' @param topic.min The smallest number of topics to consider for each model. Defaults to 5.
+#' @param topic.max The largest number of topics to consider for each model. Defaults to 55.
+#' @param model.kill The number of models with decreasing bayes factor fit before choosing the best model. A lower number
+#' tend to choose topic models with fewer topics, while a higher number may choose more topics. See the maptpx topics
+#' function package for more details. Defaults to 3.
+#' 
+#' @return The number of topics chosen for each time interval
+#'
+#' @examples
+#' \dontrun{time = as.POSIXct('2015-04-24 12:11', tz = "GMT")}
+#' \dontrun{model_time_points(tweets.df, time, difference = 1.5, num.steps = 96, topic.min = 5, 
+#' topic.max = 10, model.kill = 4)} 
+#'
+#' @export
+model_time_points = function(tweets.df, start.time, difference, num.steps, topic.min = 5, 
+                             topic.max = 55, model.kill = 3) {
+  steps = difference*60*60*0:num.steps
+  times = start.time + steps
+  return(sapply(times, model_time_point, tweets.df = tweets.df, difference = difference, topic.min = topic.min,
+                topic.max = topic.max, model.kill = model.kill))
+}
+
+model_time_point = function(start.time, tweets.df, difference, topic.min, topic.max, model.kill) {
+  text = dplyr::filter(tweets.df, created_at >= start.time & created_at <= start.time+(difference*60*60))$text
+  if(length(text) == 0) {
+    print("No Tweets during this time period")
+    return(0)
+  }
+  
+  corp = tm::Corpus(tm::VectorSource(text))
+  dtm = tm::DocumentTermMatrix(corp)
+  
+  dtm = tm::removeSparseTerms(dtm, 0.999)
+  dtm.dense = as.matrix(dtm)
+  doc.length = rowSums(dtm.dense)
+  dtm = dtm[doc.length > 0,]
+  testmap = maptpx::topics(dtm, K = c(topic.min:topic.max), kill = model.kill)
+  save(testmap, file = paste("./maptpx_model/", start.time, testmap$K, "topic model", sep = " "))
+  
+  vocab = colnames(dtm)
+  doc.length = doc.length[which(doc.length != 0)]
+  term.frequency = colSums(dtm.dense)
+  term.frequency = term.frequency[which(term.frequency != 0)]
+  theta = testmap$omega
+  phi = t(testmap$theta)
+  
+  json <- LDAvis::createJSON(phi = phi,
+                     theta = theta, 
+                     doc.length = doc.length,
+                     vocab = vocab,
+                     term.frequency = term.frequency)
+  LDAvis::serVis(json, out.dir = paste("./maptpx_vis/", start.time, "maptpx", testmap$K, "topic model", sep = " "),
+         open.browser = FALSE)
+  
+  return(testmap$K)
 }
 
 #' 5634 Tweets about Ferguson
@@ -293,3 +384,5 @@ make_tweet_df = function(tweetdir) {
 #' 
 #' @source Downloaded using the streamR filterstream function.
 "unlocatedtweets"
+
+
--- README.md
+++ README.md
@@ -1,19 +1,30 @@
-This is the code for handling tweet files downloaded using the streamR package. This package prepares this files for future topic modeling, and removes undesired variables. If the time stamps are included, they are converted into an R friendly format. If coordinates are included, this package can also label each tweet with the state it was sent from.
+This is the code for handling tweet files downloaded using the streamR package. This package prepares this files for future topic modeling, and removes undesired variables. If the time stamps are included, they are converted into an R friendly format. If coordinates are included, this package can also label each tweet with zip codes. Finally, this package can also
+be used to fit a series of topic models to a dataframe of time stamped Tweets.
 
-###Installation  
+###Installation
 
-This package can be downloaded directly from github as follows:  
+This package can be downloaded directly from github as follows:
 
-install.packages(c("qtl", "htmlwidgets", "devtools"))  
-library(devtools)  
-install_github("rturn/parseTweetFiles")  
+install.packages(c("qtl", "htmlwidgets", "devtools"))
+library(devtools)
+install_github("rturn/parseTweetFiles")
 
 ###Examples
 
-For most uses, this package is easy to use. Just place all tweets you want to edit in one folder, and run the process.files functions. If necessary, clean_tweets and locate_tweets can be used independently.
+For most uses, this package is easy to use. Just place all tweets you want to edit in one folder, and run the process.files functions. If necessary, clean.tweets and locate.tweets can be used independently.
 
 ```{r, eval = FALSE}
-process_files("~/Documents/RawTweets", "~/Documents/EditedTweets")
-process_files("~/Documents/RawTweets", "~/Documents/EditedTweets", loc = TRUE, 
-    vars = c("text", "time_zone"), tz = c("Jerusalem", "NA"), stoplist = stoplist)
+process.files("~/Documents/RawTweets", "~/Documents/EditedTweets")
+process.files("~/Documents/RawTweets", "~/Documents/EditedTweets", loc = TRUE, 
+vars = c("text", "time_zone"), tz = c("Jerusalem", "NA"), stoplist = stoplist)
+```
+
+For fitting topics models first a maptpx_model and maptpx_vis folder have to be created in the current directory. The data
+frame to fit models to must have a created_at column with time stamps in the posixct format. After that the model fitting
+is one command, and the output is saved to the created directories.
+
+```{r, eval = FALSE}
+step = 1.5
+time = as.POSIXct('2015-04-24 12:11', tz = "GMT")
+model_time_points(english.tweets.df, time, step, 96, 2, 20)
 ```
